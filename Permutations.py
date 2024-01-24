ba11f7f32859b6dd2c9a70bf4a60785501d24c5e
vineeshagandhe25 <vineeshagandhe25@gmail.com>
2024-01-24 20:30:57 +0530

    Permutations code modified to handle palindrome like numbers

--- Permutations.py
+++ Permutations.py
@@ -1,21 +1,20 @@
 # Given an integer  print all of its permutations 
-dup=0 # variable used to avoid duplicate permutations
 
 def permutations(arr,first,last):  #  function to generate  permutations
     # This code generates all the permutations of a number and no of permutations are N! where N is no of digits in input integer
     # Time Complexity --- O(N!)
-    global dup
-    if first==last and arr != dup:
+    if first==last:
         print(arr) # base condition (printing permutations)
-        dup=arr.copy() # copying current permutation into dup var
+
     else :
         for i in range(first,last):
-                arr[first],arr[i]=arr[i],arr[first]     # swapping eles to get permutations
-                permutations(arr,first+1,last)  # recursively calling 
-                arr[i],arr[first]=arr[first],arr[i]    # back tracking    
+                if arr[first:i].count(arr[i])==0:          
+                       arr[first],arr[i]=arr[i],arr[first]     # swapping eles to get permutations
+                       permutations(arr,first+1,last)  # recursively calling 
+                       arr[i],arr[first]=arr[first],arr[i]    # back tracking    
 
 
-num=temp=-1234
+num=temp=333
 res=[] # List to store digits of input integer
 if num<0:
      print("Given integer is a negative num plz provide a positive num")
