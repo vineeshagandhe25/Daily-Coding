6ca30a2b654748988d58fc1213dc995cb9295007
vineeshagandhe25 <vineeshagandhe25@gmail.com>
2024-01-28 20:13:25 +0530

    permutations

--- Permutations2.py
+++ Permutations2.py
@@ -1,21 +1,21 @@
 # Total number of permutations with duplication allowed. 
-
+count=0  #  varible to count no of permutations
 
 def permutations(arr,length,string=''):  #  function to generate  permutations
-    # This code generates all the permutations of a number and no of permutations are N! where N is no of digits in input integer
-    # Time Complexity --- O(N!)
-    
+    # This code generates all the permutations of a number and no of permutations are N^N where N is no of digits in input integer
+    # Time Complexity --- O(N^N)
+    global count
     if length==0:
         print(string) # base condition (printing permutations)
         string=''
+        count+=1 
         
-
     else :
         for i in  range(len(arr)):
-            if arr[0:i].count(arr[i])==0:       
-                permutations(arr,length-1,string+str(arr[i]))
+            if arr[0:i].count(arr[i])==0:     # To skip duplicate permutations  
+                permutations(arr,length-1,string+str(arr[i])) # Recursive calling
 
-num=temp=121
+num=temp=111
 res=[] # List to store digits of input integer
 if num<0:
      print("Given integer is a negative num plz provide a positive num")
@@ -29,6 +29,7 @@ else:
 
         
     permutations(res,len(res))
+    print(count)
     
 
 # * Time Complexity --- O(N^N) where N is no of digits in input integer 
