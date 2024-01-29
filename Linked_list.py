9919d75a33f921fd2272f6d1775be90e60771c2a
vineeshagandhe25 <vineeshagandhe25@gmail.com>
2024-01-29 21:03:06 +0530

    Linked list code updated ,added tail node to insert elements into list

--- Linked_list.py
+++ Linked_list.py
@@ -8,18 +8,16 @@ class Node:  # This class is used for linkedlist to consists one is data field a
 class Linkedlist:
 	def __init__(self):
 		self.head = None  # Head pointer  
+		self.tail=None    # Tail pointer
 		
 	def insert(self,data): # Function to insert data at end of linked list
 		new_node = Node(data) # Creating new node
 		if self.head is None:
-			self.head = new_node
+			self.head = new_node # Initially making new node as head and tail node
+			self.tail= new_node
 			return
-
-		current_node = self.head  # starting from head node and traversing through list using current_node
-		while(current_node.next):  # Traverse till last node and inserting node there
-			current_node = current_node.next
-
-		current_node.next = new_node
+		self.tail.next=new_node
+		self.tail=new_node  # Making new node as tail node
 			
 	def printLinkedList(self):
 		current_node = self.head # starting from head node and traversing through list using current_node
@@ -34,10 +32,4 @@ for i in arr:  # Time Complexity --- O(N) where N is length of arr
 ll.printLinkedList()	# To print LinkedList
 
 # *Time Complexity --- O(N) where N is length of arrr
-# *Time Complexity ---O(1) constant no of variables used.
-	
-			
-    
-			
-    
-		
+# *Time Complexity ---O(1) constant no of variables used.
