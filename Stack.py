d163c9f7e15770b299adcb17f6dddd9f00e48f51
vineeshagandhe25 <vineeshagandhe25@gmail.com>
2024-02-09 21:10:59 +0530

    Stack

---
+++ Stack.py
@@ -0,0 +1,34 @@
+class Stack:
+    def __init__(self):
+        self.items = []
+
+    def is_empty(self):
+        return self.items == []
+
+    def push(self, item):
+        self.items.append(item)
+
+    def pop(self):
+        if not self.is_empty():
+            return self.items.pop()
+        else:
+            return "Stack is empty"
+
+    def peek(self):
+        if not self.is_empty():
+            return self.items[-1]
+        else:
+            return "Stack is empty"
+
+    def size(self):
+        return len(self.items)
+
+stack = Stack()
+print(stack.is_empty())  
+
+stack.push(1)
+stack.push(2)
+stack.push(3)
+print(stack.peek())
+print(stack.pop()) 
+print(stack.pop())
