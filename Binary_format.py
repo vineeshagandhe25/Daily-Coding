bbc845f34b7c5f2a9d2eb5803f3da96158e854e2
vineeshagandhe25 <vineeshagandhe25@gmail.com>
2024-03-10 21:17:50 +0530

    added Time and Space Complexities , added comment and modified code(defined a method to find binary format)

--- Binary_format.py
+++ Binary_format.py
@@ -1,14 +1,20 @@
 #Q:Given an integer n, find its binary format and store it in an array.
-num=1234567890
-arr=[]
-while num>0:
-    r=num%2
-    arr.append(r)
-    num=num//2
-#In arr the binary digits are stored in reverse order so to get correct order we using res_arr
-res_arr=[0]*len(arr) 
-j=0
-for i in range(len(arr)-1,-1,-1):
-    res_arr[i]=arr[j]
-    j+=1
-print(res_arr)
+
+# code to find binary format
+def binaryFormat(num): 
+    res_arr=[]  # resultant binary format array
+
+    while num>0:   # Time Complexity --- O(N)  where N is the no.of digits in num
+       
+       # code to get binary format
+       rem=num%2
+       res_arr.insert(0,rem)  # inserting from left to right 
+       num=num//2
+
+    return res_arr
+
+num=12
+print(binaryFormat(num))  # printing result by calling function
+
+ # Time Complexity --- O(N)  where N is the no.of digits in num
+ # Space Complexity --- O(logM) where M is the input num
