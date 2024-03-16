6b22bcec5e91cf2aa03642d4a192e73857cd2e7f
vineeshagandhe25 <vineeshagandhe25@gmail.com>
2024-03-16 20:56:09 +0530

    Added time and space complexities and added comments too

--- min_max.py
+++ min_max.py
@@ -1,8 +1,11 @@
+# python to find min and max elements from given array.
+
+# function to find min and max eles
 def Min_Max(a,i,j,min,max):
-    if i == j:
+    if i == j: # code for single ele of array
         max=min=a[i]
         return max,min
-    elif i == (j-1):
+    elif i == (j-1): # code for comparing two eles
         if a[i] < a[j]:
             max=a[j]
             min=a[i]
@@ -11,8 +14,9 @@ def Min_Max(a,i,j,min,max):
             max=a[i]
             min=a[j]
             return max,min
-    else:
+    else: 
         mid=(i+j)//2
+        # finding mid and recursively calling functions to get min and max by comparing with exsiting min and max
         max1,min1=Min_Max(a,i,mid,min,max)
         max2,min2=Min_Max(a,mid+1,j,min,max)
         if min1<min2:
@@ -27,3 +31,6 @@ def Min_Max(a,i,j,min,max):
         
 a=[34,56,2,90,3]
 print(Min_Max(a,0,4,a[0],a[0]))
+
+# * Time COmplexity --- O(N) # N is length of input array
+# * Space COmplexity --- O(N) # N is length of input array
