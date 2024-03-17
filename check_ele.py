bc8241a42137445f82b3150563fc4bbb81d1e611
vineeshagandhe25 <vineeshagandhe25@gmail.com>
2024-03-17 21:36:07 +0530

    Added comments

--- check_ele.py
+++ check_ele.py
@@ -1,7 +1,12 @@
-words=['vinni','vinnu','vineesha','shanvi']
+# check whether given character present in given words array or not .If yes then store that index in resultant array.
+
+# function to check the ele in words
+def check(words,key):
+    res_arr=[]
+    for i in words:
+      if key in i:
+          res_arr.append(words.index(i))
+    return res_arr
+
+words=['vinni','vinnu','vineesha','shanvi'] 
 key='v'
-res_arr=[]
-for i in words:
-    if key in i:
-        res_arr.append(words.index(i))
-print(res_arr)
