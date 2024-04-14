dbf285e539b68b25cad06bf2d420df8c1dfd3043
vineeshagandhe25 <vineeshagandhe25@gmail.com>
2024-04-14 16:44:19 +0530

    finding unique pairs of difference k

--- UniquePairs2.py
+++ UniquePairs2.py
@@ -4,26 +4,29 @@ A: [1, 3, 5, 7, 10]
 k: 2
 Answer: 3'''
 
-'''Answer: This problem is solved using hashing concept.
-Sets are often used in situations where you need to quickly test for membership or eliminate duplicates from a collection.
-In Python, the underlying data structure of a set is a hash table (also known as a hash map). 
-This data structure is optimized for fast lookup, insertion, and deletion of elements. 
-Each element in the set is hashed, and the hash value is used to determine the location in memory where the element is stored.'''
-
 def findPairs(arr,k):
 
     n=len(arr)  # Time Complexity of len() is O(1)
-    hashset=set(arr)  # Time Complexity of set() is O(n) (as we are passing n iterable eles)
+    i=0
+    j=i+1
     res=0
-    if k == 0:
-        return 
     
-    # The time complexity of 'in' operation with sets in python is O(1) . 
-    for i in range(n):  # Time Complexity --- O(n) where n is len(arr)
-        if arr[i] + k  in hashset:
+    if k == 0:
+        return
+    while i < j  and j < n : 
+        if (arr[i] - arr[j]) == k:
             res += 1
+            i += 1
+            j += 1
 
-    return res 
+        elif  (arr[i] - arr[j]) > k:
+            i += 1
+            j += 1
+
+        else:
+            j += 1      
+
+    return res
 
 arr=[1,2,3,4,5]         # test cases:
 k=-1                    # negative k
