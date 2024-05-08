e5e08b328134a0a7f04bdc0817f8fd181bff205b
vineeshagandhe25 <vineeshagandhe25@gmail.com>
2024-05-08 18:35:46 +0530

    code changed to get correct output

--- OBST.py
+++ OBST.py
@@ -1,36 +1,25 @@
-# Optimal Binary Search Tree
-def obst(keys,freq):
+def obst(keys, freq):
     n = len(keys)
-    res = [[0 for _ in range(n+1)] for _ in range(n+1)]
-    
-    
-    for i in range(n+1):
-        for j in range(n+1):
-            if i == j or j < i :
-                res[i][j]=0
-            elif i+1 == j :
-                res[i][j]=freq[j-1]
-    print(res)            
-    for i in range(2,n+1):
-        for j in range(i,n+1):
-            if i == j or j < i :
-                res[i][j]=0
-            elif i+1 == j :
-                res[i][j]=freq[j-1]
-            else :
-                sum=0
-                for k in range(i+1,j+1):
-                    sum+=freq[k-1]
-                cost = float('inf')
-                for k in range(i+1,j+1):
-                    temp=(res[i][k-1]+res[k][j])
-                    #print(k,temp)
-                    if temp < cost :
-                        cost= temp
-                res[i][j]=cost+sum 
-                #print(cost,sum)                   
+    res = [[0 for _ in range(n + 1)] for _ in range(n + 2)]
+
+    for i in range(n + 1):
+        for j in range(n + 1):
+            if i == j - 1:
+                res[i][j] = freq[j - 1]
+
+    for l in range(2, n + 2):
+        for i in range(1, n - l ):
+            j = i + l - 1
+            res[i][j] = float('inf')
+            cum_freq = sum(freq[i - 1:j])
+            for r in range(i, j + 1):
+                t = res[i][r - 1] + res[r + 1][j]
+                if t < res[i][j]:
+                    res[i][j] = t
+            res[i][j] += cum_freq
+
     return res
 
 a = [1, 2, 3, 4]
 freq = [4, 2, 6, 3]
-print(obst(a,freq))
+print(obst(a, freq))
