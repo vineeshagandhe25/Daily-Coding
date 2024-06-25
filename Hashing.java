d2a43582515b8b47d9ca2f0270d59c89e043d7b6
vineeshagandhe25 <vineeshagandhe25@gmail.com>
2024-06-25 21:53:39 +0530

    finding two numbers whose sum = k, using the hashing concept.

---
+++ Hashing.java
@@ -0,0 +1,37 @@
+//Given an array, find two numbers whose sum = k, using the hashing concept. Print indices of those two elements.
+
+import java.util.*;
+public class Hashing 
+{
+    public static void main(String[] args)
+    {
+        int arr[]={1,2,3,4,5};
+        HashMap<Integer,Integer> input = new HashMap<>();
+        // Adding numbers as keys and corresponding values are their index.
+        for(int i=0;i<arr.length;i++)
+        {
+            input.put(arr[i],i);
+        }
+        
+        int k=5;
+        findIndices(input, k);
+        
+    }
+   //Function to find Indices 
+    public static void findIndices(HashMap<Integer,Integer> map ,int k)
+    {
+        System.out.println("The indices are :");
+
+        for(Integer num1 : map.keySet())
+        {
+            if(map.containsKey(k-num1))
+            {
+             int num2 =k-num1;   
+             System.out.println(map.get(num1)+" "+map.get(num2)); 
+            }
+        }
+    }
+}
+
+// Time Complexity --- O(N) where N is the length of array.
+// Space Complexity --- O(N) where N is the length of array.
