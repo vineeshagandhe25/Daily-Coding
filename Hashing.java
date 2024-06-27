6f89b1a1262374039705c72c03d118b26482b402
vineeshagandhe25 <vineeshagandhe25@gmail.com>
2024-06-27 21:08:03 +0530

    Modified code to print output in pair form

--- Hashing.java
+++ Hashing.java
@@ -5,17 +5,17 @@ public class Hashing
 {
     public static void main(String[] args)
     {
-        int arr[]={2,3,3,8,9};
+        int arr[]={2,2,3,3,5,0,5,4,4,1};
         
         HashMap<Integer,List<Integer>> input = new HashMap<>();
         // Adding numbers as keys.
-        for(int i=0;i<arr.length;i++)
+        for(int i=0;i<arr.length;i++) // Time Complexity - O(N)
         {
            
             input.put(arr[i],new ArrayList<>());
         }
-        // and corresponding values are there indices in list format .
-        for(int i=0;i<arr.length;i++)
+        // and corresponding values are there indices in list .
+        for(int i=0;i<arr.length;i++) // Time Complexity - O(N)
         {
            
             input.get(arr[i]).add(i);
@@ -29,17 +29,38 @@ public class Hashing
     public static void findIndices(HashMap<Integer,List<Integer>> map ,int k)
     {
         System.out.println("The indices are :");
-        for(Integer num1:map.keySet())
+        for(Integer num1:map.keySet()) // Time Compleixty - O(N)
         {
             if(map.containsKey(k-num1))
             {
                 int num2= k- num1;
-                System.out.println(map.get(num1)+" "+map.get(num2));
+                List<Integer> li1=map.get(num1); 
+                List<Integer> li2=map.get(num2);
+                Iterator<Integer> itr1=li1.iterator();  //defining iterators to list and printing value's list in order to print indices as pair. 
+                Iterator<Integer> itr2=li2.iterator();
+                int i = 0;
+                int j = 0;
+
+                while(itr1.hasNext() || itr2.hasNext()) // Time Compleixty - O(M) where M is maxSize(it1,itr2)
+                {
+                    
+                    if(itr1.hasNext())
+                    {
+                      i=itr1.next();
+                    }
+
+                    if(itr2.hasNext())
+                    {
+                      j=itr2.next();
+                    }
+
+                    System.out.println(i+" "+j);
+                }
             }
-        }
+        } 
         
     }
 }
 
-// Time Complexity --- O(N) where N is the length of array.
-// Space Complexity --- O(N) where N is the length of array.
+// Time Complexity --- O(N*M) where N is the length of hashmap and M is maxSize(it1,itr2).
+// Space Complexity --- O(N) where N is the length of hashmap.
