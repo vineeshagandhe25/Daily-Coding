23e9cc093c026fe8e2c7b137b9c2a0847637eafc
vineeshagandhe25 <vineeshagandhe25@gmail.com>
2024-07-24 21:12:33 +0530

    Salesforce question

---
+++ Salesforceq1.java
@@ -0,0 +1,11 @@
+/*
+Input:
+N: integer- number of dices
+M: integer- number of faces on each dice
+X: integer- The target sum.
+Output: 
+An integer that represents number of ways to achieve the sum X when all N dice are rolled
+ */
+public class Salesforceq1 {
+    
+}
