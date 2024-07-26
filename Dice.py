d51a3d9230d477e94aacf563267814725b73109d
vineeshagandhe25 <vineeshagandhe25@gmail.com>
2024-07-26 21:14:13 +0530

    Dice challenge - added if condition and interchanged M and N values.

--- Dice.py
+++ Dice.py
@@ -7,18 +7,20 @@ Output:
 An integer that represents number of ways to achieve the sum X when all N dice are rolled '''
 
 def num_ways(N, M, X):
-    dp = [[0] * (X + 1) for _ in range(N + 1)]
+    dp = [[0] * (X + 1) for _ in range(M + 1)]
     dp[0][0] = 1
+    if M == 0 or X == 0:
+        return 0
 
-    for i in range(1, N + 1):
+    for i in range(1, M + 1):
         for j in range(1, X + 1):
-            for k in range(1, min(j, M) + 1):
+            for k in range(1, min(j, N) + 1):
                 dp[i][j] += dp[i - 1][j - k]
 
-    return dp[N][X]
+    return dp[M][X]
 
-N = 3  
-M = 6  
-X = 7  
+N = 6
+M = 3
+X = 12 
 result = num_ways(N, M, X)
 print(result)
