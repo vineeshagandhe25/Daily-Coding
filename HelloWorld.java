33ca27737d7cc78d443687ecdb8052e99389257f
vineeshagandhe25 <vineeshagandhe25@gmail.com>
2024-07-29 20:00:45 +0530

    Printing HelloWorld without using string literals.

---
+++ HelloWorld.java
@@ -0,0 +1,17 @@
+// Print "HelloWorld" without using string literals .
+public class HelloWorld {
+    public static void main(String[] args) {
+        // Use ASCII arithmetic to print "HelloWorld" without string literals
+
+        System.out.print((char) (72));  // 'H'
+        System.out.print((char) (101)); // 'e'
+        System.out.print((char) (108)); // 'l'
+        System.out.print((char) (108)); // 'l'
+        System.out.print((char) (111)); // 'o'
+        System.out.print((char) (87));  // 'W'
+        System.out.print((char) (111)); // 'o'
+        System.out.print((char) (114)); // 'r'
+        System.out.print((char) (108)); // 'l'
+        System.out.print((char) (100)); // 'd'
+    }
+}
