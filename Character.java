aba552bf026d3900e1514283a200be31232245af
vineeshagandhe25 <vineeshagandhe25@gmail.com>
2024-08-12 21:04:02 +0530

    Started a question to solve.

---
+++ Character.java
@@ -0,0 +1,14 @@
+// Wrtie a program to give the following output for the given input
+/*Input :a1b10
+ * Output : abbbbbbbbbb
+ */
+class Character {
+    public static void main(String[] args) {
+        
+    }
+    public static void printChars(String str){
+        for(int i=0;i<str.length();i++){
+            
+        }
+    }
+}
