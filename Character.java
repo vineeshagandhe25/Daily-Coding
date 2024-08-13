f5679b5b6a2299827e5c882f1ea4c7e16c76483e
vineeshagandhe25 <vineeshagandhe25@gmail.com>
2024-08-13 21:19:29 +0530

    Added code to printChars .

--- Character.java
+++ Character.java
@@ -2,13 +2,34 @@
 /*Input :a1b10
  * Output : abbbbbbbbbb
  */
-class Character {
+class CharacterDemo {
     public static void main(String[] args) {
-        
+        String input = "a1b10";
+        printChars(input);
     }
-    public static void printChars(String str){
-        for(int i=0;i<str.length();i++){
+
+    public static void printChars(String str) {
+        for (int i = 0; i < str.length(); i++) {
+            char ch = str.charAt(i);
             
+            
+            if (Character.isLetter(ch)) {
+                System.out.print(ch);
+            }
+           
+            else if (Character.isDigit(ch)) {
+                int count = ch - '0'; 
+               
+                while (i + 1 < str.length() && Character.isDigit(str.charAt(i + 1))) {
+                    i++;
+                    count = count * 10 + (str.charAt(i) - '0');
+                }
+               
+                char previousChar = str.charAt(i - 1);
+                for (int j = 1; j < count; j++) {
+                    System.out.print(previousChar);
+                }
+            }
         }
     }
 }
