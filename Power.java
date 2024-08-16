6778d2d6606bc8d8dc1fb9b8caeb3f5ba7316dd2
vineeshagandhe25 <vineeshagandhe25@gmail.com>
2024-08-16 20:17:20 +0530

    Added Time complexity ,changed the return type and covered the corner cases.

--- Power.java
+++ Power.java
@@ -2,22 +2,30 @@
 public class Power {
     public static void main(String[] args) {
         int base = 5;
-        int exponent = 1;
-        power(base,exponent);
+        int exponent = 2;
+        System.out.println(power(base, exponent));
+        // Coner Cases
+        System.out.println(power(base, 0));
+        System.out.println(power(0, exponent));
+        System.out.println(power(0, 0));
+        System.out.println(power(base, 1));
+        System.out.println(power(base, -2));
     }
 
-    public static void power(int base,int exponent) {
-        int result=1;
+    public static float power(int base, int exponent) {
+        float result = 1;
         int limit = Math.abs(exponent);
-        
-        for(int i=0;i<limit;i++) {
+
+        for (int i = 0; i < limit; i++) { // Time Complexity --- O(N) where N is value of Limit
             result *= base;
         }
-        if(exponent < 0) {
-            System.out.println(1/result);
-        }
-        else {
-            System.out.println(result);
+        if (exponent < 0) {
+            return 1 / result;
+        } else {
+            return result;
         }
     }
 }
+
+// Time Complexity --- O(N) where N is value of Limit.
+// Space Complexity --- O(1)
