d36c4a03a9602ff87c239f15e54a782acee281ab
vineeshagandhe25 <vineeshagandhe25@gmail.com>
2024-08-27 21:29:04 +0530

    Conversion of binary string to original string.

---
+++ BinaryString.java
@@ -0,0 +1,18 @@
+/*You are given a binary string consisting of a sequence of 1's and 0's. This binary string is an encoded version of an English Word.
+ * Each uppercase letter in the world is represented by a sequence of 1's of varying lenghts , while 0's act as separators between these sequences.
+ * Task is to decode the binary string and determine the original word.
+ * 
+ * INPUT : 11101011110
+ * OUTPUT : CAD 
+ */
+
+ public class BinaryString {
+    public static void main(String[] args) {
+        
+    }
+
+    //public static String findString(String binaString) {}
+    
+ }
+
+ /* To solve it we can use haspmap in order to optimise the search operation , here keys will be the alphabets and corresponding values are 1's  sequence*/
