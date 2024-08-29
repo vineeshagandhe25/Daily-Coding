25b6333efa13bb538d990613c9965b3a20d40223
vineeshagandhe25 <vineeshagandhe25@gmail.com>
2024-08-29 21:31:06 +0530

    working on palindromic substrings of Strings.

---
+++ PalindromicSubStrings.java
@@ -0,0 +1,12 @@
+/*Problem statement
+You have been given a string STR. Your task is to find the total number of palindromic substrings of STR.
+
+Example :
+If the input string is "abbc", then all the possible palindromic substrings would be: ["a", "b", "b", c", "bb"] and hence, 
+the output will be 5 since we have 5 substrings in total which form a palindrome. */
+
+public class PalindromicSubStrings {
+    public static void main(String[] args) {
+
+    }
+}
