2cdb12f2fa146dcf2d553c9ce98fc56f81fc1e8f
vineeshagandhe25 <vineeshagandhe25@gmail.com>
2024-09-25 21:09:42 +0530

    Added Time and Space Complexities.

--- Num_to_words_II.py
+++ Num_to_words_II.py
@@ -1,6 +1,6 @@
 #Q:Convert a given integer into words
 num=99999
-ones=['one','two','three','four','five','six','seven','eight','nine']
+ones=['one','two','three','four','five','six','seven','eight','nine']  # ones place
 teens = ["ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen"]
 tens = [ "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty","ninety"]
 if num==0:
@@ -9,7 +9,7 @@ else:
  if num<0:
     print("invalid")
  output_string=''
- if num>=10000000:
+ if num>=10000000: # crores
     r=num//10000000
     if r>=10 and r<20:
         output_string+=teens[r-10]+" "+"crore"+" "
@@ -21,7 +21,7 @@ else:
     elif r==0:
         output_string+=" "+"crore"+" "       
     num=num%10000000  
- if num>=100000:
+ if num>=100000: #lakhs
     r=num//100000
     if r>=10 and r<20:
         output_string+=teens[r-10]+" "+"lakh"+" " 
@@ -33,7 +33,7 @@ else:
     elif r==0:
         output_string+=" "+"lakh"+" "    
     num=num%100000
- if num>=1000:
+ if num>=1000: #thousands
     r=num//1000
     if r>=10 and r<20:
         output_string+=teens[r-10]+" "+"thousand"+" " 
@@ -47,7 +47,7 @@ else:
         output_string+=" "+"thousand"+" "
     num=num%1000
     
- if num>=100:
+ if num>=100: # hundred
         output_string+=ones[r//100-1]+"hundred"+" "
         r=num%100
         if 10 <= r < 20:
@@ -61,3 +61,6 @@ else:
 
     
  print(output_string)
+
+# Time Complexity --- O(1)
+# Space Complexity --- O(1)
