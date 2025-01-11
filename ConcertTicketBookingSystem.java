70e32d24ae993e0bcb47dcc4c0b454780452c6e4
vineeshagandhe25 <vineeshagandhe25@gmail.com>
2025-01-11 19:12:33 +0530

    Identified classes in given case study Concert Ticket Booking System.

---
+++ ConcertTicketBookingSystem.java
@@ -0,0 +1,32 @@
+// Concert Ticket Booking System
+
+// Concert class to hold concert details 
+class Concert {
+    
+}
+
+// User class to hold user details 
+class User {
+    
+}
+
+// Booking class to hold booking details 
+class Booking {
+   
+}
+
+// Payment  class to hold payment details 
+class Payment {
+    
+}
+
+// Waiting List class to hold waiting list details 
+class WaitingList {
+    
+}
+
+public class ConcertTicketBookingSystem {
+    public static void main(String[] args) {
+        // code 
+    }
+}
