80232c48f54417f26e832737b7fd298e07c63001
vineeshagandhe25 <vineeshagandhe25@gmail.com>
2025-06-17 16:44:20 +0530

    GCD and LCM using the Euclidean Algorithm

---
+++ GCDLCM.java
@@ -0,0 +1,25 @@
+public class GCDLCM {
+    // Function to find GCD using Euclidean algorithm
+    public static int gcd(int a, int b) {
+        while (b != 0) {
+            int temp = b;
+            b = a % b;
+            a = temp;
+        }
+        return a;
+    }
+
+    // Function to find LCM using GCD  Using relation between GCD and LCD 
+    public static int lcm(int a, int b) {
+        return (a * b) / gcd(a, b);
+    }
+
+    public static void main(String[] args) {
+        int a = 36, b = 60;
+
+        System.out.println("GCD of " + a + " and " + b + " is: " + gcd(a, b));
+        System.out.println("LCM of " + a + " and " + b + " is: " + lcm(a, b));
+    }
+}
+// Time Complexity --- O(log(min(a, b)))
+// Space Complexity --- O(1)
