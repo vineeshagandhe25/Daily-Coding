c0d901a7fe64bdac540361cbab90e905f5754d29
vineeshagandhe25 <vineeshagandhe25@gmail.com>
2025-08-26 19:09:42 +0530

    Wroked on KMP Algorithm

---
+++ KMP.java
@@ -0,0 +1,61 @@
+public class KMP {
+    private static int[] computeLPS(String pattern) {
+        int m = pattern.length();
+        int[] lps = new int[m];
+
+        int len = 0;
+        int i = 1;
+
+        while (i < m) {
+            if (pattern.charAt(i) == pattern.charAt(len)) {
+                len++;
+                lps[i] = len;
+                i++;
+            } else {
+                if (len != 0) {
+                    len = lps[len - 1];
+                } else {
+                    lps[i] = 0;
+                    i++;
+                }
+            }
+        }
+        return lps;
+
+    }
+
+    public static void searchString(String text, String pattern) {
+        int n = text.length();
+        int m = pattern.length();
+
+        int[] lps = computeLPS(pattern);
+
+        int i = 0;
+        int j = 0;
+
+        while (i < n) {
+            if (pattern.charAt(j) == text.charAt(i)) {
+                i++;
+                j++;
+            }
+            if (j == m) {
+                System.out.println("Pattern found at " + (i - j));
+                j = lps[j - 1];
+            }
+
+            else if (i < n && pattern.charAt(j) != text.charAt(i)) {
+                if (j != 0) {
+                    j = lps[j - 1];
+                } else
+                    i++;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        String text = "ababcabcabababd";
+        String pattern = "ababd";
+
+        searchString(text, pattern);
+    }
+}
